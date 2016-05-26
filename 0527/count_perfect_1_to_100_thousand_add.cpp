b227ff4ec8725c061b5bf3ae746ac99beefc1f32
MU-Software <MU-Software@users.noreply.github.com>
2016-05-27 06:22:38 +0900

    Update count_perfect_1_to_100_thousand_add.cpp

--- 0527/count_perfect_1_to_100_thousand_add.cpp
+++ 0527/count_perfect_1_to_100_thousand_add.cpp
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int is_perfect (int n) {
+	int sum2 = 0;
+	for (int i = 1; i<n; i++) if (n % i == 0) sum2 += i;
+	if (sum2 == n) return 1;
+	else return 0;
+}
+
+int len_calc(int numInput) {
+	int length = 0;
+	do {
+		numInput = int(numInput / 10);
+		length++;
+	} while (numInput > 0);
+	return length;
+}
+
+int main (void) {
+	//1에서 10만까지, 만약 is_perfect(i)가 참이면
+	for (int i = 1; i <= 100000; i++) if (is_perfect(i)) {
+		//i의 약수 출력
+		for (int k = 1; k < i; k++) if (!(i % k)) {
+			printf(" %d", k);
+			//줄맞춤
+			for (int lining = 5; lining - len_calc(k) > 0; lining--) putchar(' ');
+			//일정한 규칙이 있다.
+			printf((i / 2 == k) ? "= " : "+");
+		}
+		//해당되는 수 출력
+		printf("%d\n", i);
+	}
+	return 0;
+}
